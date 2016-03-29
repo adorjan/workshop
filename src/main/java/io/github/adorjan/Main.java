06a40381baef5ac1db0c1e1e6745130c1e9ef8f3
Adorjan Nagy <adorjan_nagy@epam.com>
2016-03-29 11:04:42 +0200

    Add logging

--- src/main/java/io/github/adorjan/Main.java
+++ src/main/java/io/github/adorjan/Main.java
@@ -3,17 +3,21 @@
  */
 package io.github.adorjan;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Adorjan Nagy
  *
  */
 public class Main {
+    private final static Logger LOG = LoggerFactory.getLogger(Main.class);
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello All!");
+        LOG.info("Hello All!");
     }
 
 }
