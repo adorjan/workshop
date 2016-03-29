aae3c566535a696dd10d19e6a1c2ea7cf5a3b286
Adorjan Nagy <adorjan_nagy@epam.com>
2016-03-29 10:35:14 +0200

    Initial commit

---
+++ src/main/java/io/github/adorjan/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package io.github.adorjan;
+
+/**
+ * @author Adorjan Nagy
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello All!");
+    }
+
+}
